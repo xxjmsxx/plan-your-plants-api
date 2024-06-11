ebbc7d1b8a148c0315ecb9be415d8cba8e327095
Joshua Smith <sarjsmithde@gmail.com>
2024-06-12 00:30:45 +0200

    Refactor UsersController and move session handling to SessionsController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,14 @@
 # app/controllers/users_controller.rb
 # frozen_string_literal: true
 class UsersController < ApplicationController
-  skip_before_action :authorized, only: [:create, :login]
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  skip_before_action :authorized, only: [:create]
 
-  def login
-    @user = User.find_by(email: params[:email])
-    if @user&.authenticate(params[:password])
-      token = encode_token({ user_id: @user.id })
-      render json: { user: @user, token: token }, status: :ok
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
     else
-      render json: { error: 'Invalid email or password' }, status: :unauthorized
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
@@ -25,9 +16,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :username)
-  end
-
-  def encode_token(payload)
-    JWT.encode(payload, 'your_secret_key')
   end
 end
